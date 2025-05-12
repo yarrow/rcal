6e2d01063156d0df40fc41affc6a52546186b7bd
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2025-05-11 22:47:58 -0400

    Use LocStr instead of Range<usize>
    For names and values we now use
    
        pub struct LocStr<'a> {
            loc: usize,
            val: &'a str,
        }
    
    rather than the range `start..index` that we now use to create `val`.
    This lets us keep the `unsafe{...}` blocks inside of `preparse`, while
    maintaining the ability to specify the location of errors.

--- src/preparse.rs
+++ src/preparse.rs
@@ -1,5 +1,4 @@
 //! Operations related to RFC 5545 validation.
-use bstr::{B, ByteSlice};
 
 use crate::error::PreparseError;
 use std::ops::Range;
@@ -11,7 +10,7 @@ const NO_COLON_OR_SEMICOLON: &str =
     "Property name must be followed by a colon (:) or a semicolon (;)";
 const NO_COMMA_ETC: &str =
     "Parameter value must be followed by a comma (,) or colon (:) or semicolon(;)";
-const NO_PARAMETER_NAME: &str = "No parameter name after semicolon";
+const NO_PARAM_NAME: &str = "No parameter name after semicolon";
 const NO_EQUAL_SIGN: &str = "Parameter name must be follow by an equal sign (=)";
 const UNEXPECTED_DOUBLE_QUOTE: &str = r#"unexpected double quote (")"#;
 const CONTROL_CHARACTER_IN_PARAMETER: &str =
@@ -21,38 +20,65 @@ const CONTROL_CHARACTER_IN_PROPERTY: &str =
 const UTF8_ERROR: &str = "UTF8 error";
 const NO_PROPERTY_VALUE: &str = "Content line has no property value";
 
+/// A located `str`: a substring of a larger string, along with its location in that string.
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct LocStr<'a> {
+    loc: usize,
+    val: &'a str,
+}
 #[derive(Debug, PartialEq)]
-pub struct Param {
-    name: Range<usize>,
-    values: Vec<Range<usize>>,
+pub struct Param<'a> {
+    name: LocStr<'a>,
+    values: Vec<LocStr<'a>>,
 }
 #[derive(Debug, PartialEq)]
 pub struct Prop<'a> {
-    name: Range<usize>,
-    parameters: Vec<Param>,
-    value: Range<usize>,
-    text: &'a str,
+    name: LocStr<'a>,
+    parameters: Vec<Param<'a>>,
+    value: LocStr<'a>,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum State {
+    ParamName,
+    StartParamValue,
+    ParamText,
+    ParamQuoted,
+    Value,
+    NonAscii,
+}
 pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
-    #[derive(Debug, Clone, Copy, PartialEq)]
-    enum State {
-        ParamName,
-        StartParamValue,
-        ParamText,
-        ParamQuoted,
-        Value,
-        NonAscii,
-    }
-    let mut index = 0;
-    // INVARIANT V: `v[..index]` is a valid UTF8 string
-    // Note that the concatenation of two valid UTF8 strings is also valid, so if `v[index]`
+    // INVARIANT: `v[start..index]` is a valid UTF8 string. (Implies `start <= index && index <= v.len()`)
+    //
+    // We only modify `start` by setting it to the currrent value of `index`, which establishes
+    // the invariant (since `v[start..index]` is then the empty string).
+    //
+    // Since the concatenation of two valid UTF8 strings is also valid, if `v[index]`
     // is ASCII we can increment `index` while maintaining the invariant. For a multi-byte
     // UTF8 code point we may briefly break the invariant while checking that it is indeed
     // a valid code point, but if it's not valid we'll return an error and if it is valid
     // we'll restore the invariant by setting `index` to the byte past the end of the code
     // point.
+    let mut index = 0;
+    let mut start = index;
 
+    // Given the invariant, `loc_str!(start, index)` always produces a `LocStr{loc, val}` where
+    // `v[loc]` is the start of a UTF8 code point and `val` is a valid UTF8 string — again as long
+    // as we don't call `loc_str!(start, index)` in the middle of scanning a multi-byte UTF8 code
+    // point.
+    //
+    // And we never call `loc_str!` except as `loc_str!(start, index)`
+    macro_rules! loc_str {
+        ($start: ident, $index: ident) => {{
+            debug_assert!(str::from_utf8(&v[$start..$index]).is_ok());
+            LocStr {
+                loc: $start,
+                val: unsafe { str::from_utf8_unchecked(v.get_unchecked($start..$index)) },
+            }
+        }};
+    }
+
+    // Return an error: the input doesn't correspond to the basic grammar in RFC 5545 § 3.1
     macro_rules! rfc_err {
         ($reason: expr) => {
             return Err(PreparseError { reason: $reason, valid_up_to: index, error_len: None })
@@ -63,7 +89,6 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
     if len == 0 {
         rfc_err!("Empty content line")
     }
-
     let mut state = State::NonAscii;
     while index < len {
         match v[index] {
@@ -76,10 +101,7 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
                 state = State::ParamName;
                 break;
             }
-            _ => {
-                let reason = if index > 0 { NO_COLON_OR_SEMICOLON } else { NO_PROPERTY_NAME };
-                rfc_err!(reason);
-            }
+            _ => rfc_err!(if index == 0 { NO_PROPERTY_NAME } else { NO_COLON_OR_SEMICOLON }),
         };
     }
     debug_assert!(matches!(state, State::Value | State::ParamName));
@@ -87,30 +109,16 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
     if index == 0 {
         rfc_err!(NO_PROPERTY_NAME);
     }
-    let property_name = 0..index; // Valid UTF8 by Invariant V
+    let mut param_name = LocStr::default();
+    let mut param_values = Vec::<LocStr>::new();
+    let mut parameters = Vec::<Param<'a>>::new();
+    let property_name = loc_str!(start, index);
     index += 1;
 
     let mut start = index;
-    // Invariant Start: `v[start..index]` is a valid UTF8 string. We only change `start`
-    // by setting it to the currrent value of `index`, which established the invariant (since
-    // `v[start..index]` is then the empty string), and Invariant Start is maintained when
-    // we increase `index` for the same reasons that Invariant V is maintained.
-
-    let mut param_name = 0..0;
-    let mut param_values = Vec::<Range<usize>>::new();
-    let mut parameters = Vec::<Param>::new();
-    // Invariant P: `v[param_name]` is a valid UTF8 string, and for each `value` in param_values,
-    // `v[value]` is a valid UTF8 string. We maintain this invariant by virtue of Invariant Start
-    // since we only set `param_name` to `start..index` and the only value we push to
-    // `param_values` is `start..index`
-    // Furthermore, for every `param` in `parameters` we know that `v[param.name]` is valid UTF8
-    // and `v[value]` is valid UTF8 for every `value` in `param.values`, since `param.name` only
-    // comes from `param_name` and `param.values` only comes from `param_values`, via the macro
-    // below:
-
     macro_rules! finish_parameter {
         () => {
-            if !param_name.is_empty() {
+            if !param_name.val.is_empty() {
                 parameters.push(Param {
                     name: mem::take(&mut param_name),
                     values: mem::take(&mut param_values),
@@ -118,7 +126,6 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
             }
         };
     }
-
     let mut old_state = state;
     'outer: while index < len {
         let mut this_byte = v[index];
@@ -144,7 +151,7 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
             }};
         }
 
-        // if the current byte isn't ASCII, ensure that we handle that (with code stolen from the
+        // If the current byte isn't ASCII, ensure that we handle that (with code stolen from the
         // Rust compiler), while remembering the current state in order to return to it. In effect,
         // perform a manual subroutine call.
         macro_rules! handle_non_ascii {
@@ -165,16 +172,12 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
                         b'-' | b'0'..=b'9' | b'A'..=b'Z' | b'a'..=b'z' => next_byte_or_finish!(),
                         b'=' => {
                             if index == start {
-                                rfc_err!(NO_PARAMETER_NAME)
+                                rfc_err!(NO_PARAM_NAME)
                             }
-                            param_name = start..index;
+                            param_name = loc_str!(start, index);
                             change_state_to!(State::StartParamValue);
                         }
-                        _ => {
-                            let reason =
-                                if index > start { NO_EQUAL_SIGN } else { NO_PARAMETER_NAME };
-                            rfc_err!(reason);
-                        }
+                        _ => rfc_err!(if index == start { NO_PARAM_NAME } else { NO_EQUAL_SIGN }),
                     };
                 }
             }
@@ -195,15 +198,15 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
                     }
                     b'"' => rfc_err!(UNEXPECTED_DOUBLE_QUOTE),
                     b',' => {
-                        param_values.push(start..index);
+                        param_values.push(loc_str!(start, index));
                         change_state_to!(State::StartParamValue);
                     }
                     b':' => {
-                        param_values.push(start..index);
+                        param_values.push(loc_str!(start, index));
                         change_state_to!(State::Value);
                     }
                     b';' => {
-                        param_values.push(start..index);
+                        param_values.push(loc_str!(start, index));
                         change_state_to!(State::ParamName);
                     }
                     _ => rfc_err!(CONTROL_CHARACTER_IN_PARAMETER),
@@ -215,7 +218,7 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
                     b'\t' | b' '..b'"' | b'#'..127 => next_byte_or_finish!(),
                     b'"' => {
                         start += 1;
-                        param_values.push(start..index);
+                        param_values.push(loc_str!(start, index));
                         next_byte_or_finish!();
                         match this_byte {
                             b',' => change_state_to!(State::StartParamValue),
@@ -326,15 +329,9 @@ pub fn preparse<'a>(v: &'a [u8]) -> Result<Prop<'a>, PreparseError> {
         }
     }
     debug_assert!(state != State::Value || index == v.len());
-    // So by Invariant V, `state == State::Value` implies `v` is valid UTF8
 
     match state {
-        State::Value => Ok(Prop {
-            name: property_name,
-            parameters,
-            value: start..index,
-            text: unsafe { str::from_utf8_unchecked(v) }, // Safe by Invariant V
-        }),
+        State::Value => Ok(Prop { name: property_name, parameters, value: loc_str!(start, index) }),
         State::NonAscii => panic!("BUG: should be impossible to exit loop with state == NonAscii"),
         State::ParamName | State::StartParamValue | State::ParamText | State::ParamQuoted => {
             rfc_err!(NO_PROPERTY_VALUE)
@@ -375,6 +372,7 @@ fn utf8_char_width(b: u8) -> usize {
 #[cfg(test)]
 mod test {
     use super::*;
+    use bstr::BString;
     use pretty_assertions::assert_eq;
 
     fn validate(s: &str) -> Result<(), PreparseError> {
@@ -414,10 +412,20 @@ mod test {
     #[test]
     fn property_name_required() {
         assert_eq!(error_for(":foo"), NO_PROPERTY_NAME);
+        assert_eq!(error_for("/foo"), NO_PROPERTY_NAME);
     }
     #[test]
     fn parameter_name_required() {
-        assert_eq!(error_for("Foo;=bar:"), NO_PARAMETER_NAME);
+        assert_eq!(error_for("Foo;=bar:"), NO_PARAM_NAME);
+        assert_eq!(error_for("Foo;/:"), NO_PARAM_NAME);
+    }
+
+    #[test]
+    fn must_utf8() {
+        let mut bad = BString::from("FOO:bá");
+        let len = bad.len();
+        bad[len - 1] = b'a';
+        assert_eq!(preparse(bad.as_slice()).unwrap_err().reason, UTF8_ERROR);
     }
 
     // Tests for the result returned
@@ -432,22 +440,22 @@ mod test {
         parameters: Vec<StrParam<'a>>,
         value: &'a str,
     }
-    fn as_substrings<'a>(prop: Prop<'a>) -> StrProp<'a> {
+    fn delocate<'a>(prop: Prop<'a>) -> StrProp<'a> {
         StrProp {
-            name: &prop.text[prop.name],
-            value: &prop.text[prop.value],
+            name: prop.name.val,
+            value: prop.value.val,
             parameters: prop
                 .parameters
                 .iter()
                 .map(|param| StrParam {
-                    name: &prop.text[param.name.clone()],
-                    values: param.values.iter().map(|value| &prop.text[value.clone()]).collect(),
+                    name: param.name.val,
+                    values: param.values.iter().map(|value| value.val).collect(),
                 })
                 .collect(),
         }
     }
     fn parse<'a>(text: &'a str) -> StrProp<'a> {
-        as_substrings(preparse(text.as_bytes()).unwrap())
+        delocate(preparse(text.as_bytes()).unwrap())
     }
 
     #[test]
@@ -472,11 +480,11 @@ mod test {
 
     #[test]
     fn non_ascii() {
-        let text = "FOO;BAR=íííí:béééé";
+        let text = r#"FOO;BAR=íííí,,"óu":béééé"#;
         let expected = StrProp {
             name: "FOO",
             value: "béééé",
-            parameters: vec![StrParam { name: "BAR", values: vec!["íííí"] }],
+            parameters: vec![StrParam { name: "BAR", values: vec!["íííí", "", "óu"] }],
         };
         let result = parse(text);
         assert_eq!(result, expected);
