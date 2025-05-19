2b650d8318419333a62bf7bd553c2cfa08d57f54
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2025-05-19 19:59:59 -0400

    Implement Display for PreparseError
    ...and remove the no-longer-used reason() method.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::io;
 use thiserror::Error;
 #[derive(Error, Debug)]
@@ -34,7 +35,6 @@ pub enum Problem {
     Unterminated,
 }
 #[derive(Clone, Debug, Error, PartialEq)]
-#[error("FIXME at column {valid_up_to}")] // FIXME!
 pub struct PreparseError {
     pub(crate) segment: Segment,
     pub(crate) problem: Problem,
@@ -45,48 +45,95 @@ pub(crate) const EMPTY_CONTENT_LINE: PreparseError = PreparseError {
     problem: Problem::EmptyContentLine,
     valid_up_to: 0,
 };
-impl PreparseError {
-    #[must_use]
-    pub fn reason(&self) -> &'static str {
+
+impl fmt::Display for PreparseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use Problem::*;
+        use Segment::*;
         match self.problem {
-            ControlCharacter => CONTROL_CHARACTER,
-            Utf8Error(_) => UTF8_ERROR,
-            DoubleQuote => UNEXPECTED_DOUBLE_QUOTE,
-            UnclosedQuote => "Unclosed quoted string",
-            EmptyContentLine => "Empty content line",
+            ControlCharacter => {
+                write!(f, "invalid control character at index {}", self.valid_up_to)
+            }
+            Utf8Error(error_len) => {
+                if let Some(error_len) = error_len {
+                    write!(
+                        f,
+                        "invalid utf-8 sequence of {} bytes from index {}",
+                        error_len, self.valid_up_to
+                    )
+                } else {
+                    write!(f, "incomplete utf-8 byte sequence from index {}", self.valid_up_to)
+                }
+            }
+            DoubleQuote => write!(f, "unexpected double quote (\") at index {}", self.valid_up_to),
+            UnclosedQuote => write!(f, "expected double quote (\") at index {}", self.valid_up_to),
+            EmptyContentLine => write!(f, "content line is empty"),
             Empty => match self.segment {
-                Segment::ParamName => NO_PARAM_NAME,
-                Segment::PropertyName => NO_PROPERTY_NAME,
-                Segment::PropertyValue => NO_PROPERTY_VALUE,
-                Segment::ParamValue => "BUG: parameter value can be empty",
+                ParamName => write!(f, "expected a parameter name after the semicolon (;)"),
+                PropertyName => write!(f, "content line doesn't start with a property name"),
+                PropertyValue => {
+                    write!(f, "content line does end with a property value — missing colon (:)?")
+                }
+                ParamValue => write!(
+                    f,
+                    "BUG: the error claims there's an empty parameter value, but parameter values \
+                    can be empty"
+                ),
             },
             Unterminated => match self.segment {
-                Segment::ParamName => NO_EQUAL_SIGN,
-                Segment::PropertyName => NO_COLON_OR_SEMICOLON,
-                Segment::ParamValue => NO_COMMA_ETC,
-                Segment::PropertyValue => "BUG: property value should never have this problem",
+                ParamName => write!(
+                    f,
+                    "expecting an equals sign (=) at index {}, after the parameter name",
+                    self.valid_up_to
+                ),
+                PropertyName => write!(
+                    f,
+                    "expecting a colon (:) or semicolon (;) at index {}, after the property name",
+                    self.valid_up_to
+                ),
+                ParamValue => write!(
+                    f,
+                    "expecting a a comma (,) or colon (:) or semicolon(;) at index {}, after the \
+                    parameter value",
+                    self.valid_up_to
+                ),
+                PropertyValue => write!(
+                    f,
+                    "BUG: the error claims the property value is ended by an unexpected character, \
+                    but the only candidates (ASCII control characters and invalid utf8 sequences) \
+                    have separate error messages"
+                ),
             },
         }
     }
-    #[must_use]
-    pub fn is_utf8_error(&self) -> bool {
-        matches!(self.problem, Problem::Utf8Error(_))
-    }
-    #[must_use]
-    pub fn is_control_char_error(&self) -> bool {
-        matches!(self.problem, Problem::ControlCharacter)
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_line_breaks() {
+        // Make use I ended each broken line with a line feed (and have no extra spaces)
+        use Problem::*;
+        use Segment::*;
+        let segments = [PropertyName, PropertyValue, ParamName, ParamValue];
+        let problems = [
+            ControlCharacter,
+            Utf8Error(None),
+            DoubleQuote,
+            UnclosedQuote,
+            EmptyContentLine,
+            Empty,
+            Unterminated,
+        ];
+        for p in problems {
+            for s in segments {
+                let err = PreparseError { segment: s, problem: p, valid_up_to: 0 };
+                let message = err.to_string();
+                let bad = message.find('\n').or_else(|| message.find("  "));
+                if bad.is_some() {
+                    panic!("\n{err:?}\n{message}");
+                }
+            }
+        }
     }
 }
-pub(crate) const CONTROL_CHARACTER: &str =
-    "ASCII control characters are not allowed, except tab (\\t)";
-pub(crate) const NO_COLON_OR_SEMICOLON: &str =
-    "Property name must be followed by a colon (:) or a semicolon (;)";
-pub(crate) const NO_COMMA_ETC: &str =
-    "Parameter value must be followed by a comma (,) or colon (:) or semicolon(;)";
-pub(crate) const NO_EQUAL_SIGN: &str = "Parameter name must be follow by an equal sign (=)";
-pub(crate) const NO_PARAM_NAME: &str = "No parameter name after semicolon";
-pub(crate) const NO_PROPERTY_NAME: &str = "Content line doesn't start with a property name";
-pub(crate) const NO_PROPERTY_VALUE: &str = "Content line has no property value";
-pub(crate) const UNEXPECTED_DOUBLE_QUOTE: &str = r#"unexpected double quote (")"#;
-pub(crate) const UTF8_ERROR: &str = "UTF8 error";
